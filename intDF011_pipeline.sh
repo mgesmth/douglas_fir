8cece01c6709aaa6754c1bef5589d3faaa263d2a
mgesmth <meg8130@student.ubc.ca>
2025-04-25 15:18:29 -0700

    Update and rename myasm_pipeline.sh to intDF011_pipeline.sh

--- intDF011_pipeline.sh
+++ intDF011_pipeline.sh
@@ -7,12 +7,16 @@ HIFI=/home/FCAM/msmith/hifi_data/intDF_allhifi.fastq.gz
 HIC_R1=/home/FCAM/msmith/hiC_data/allhiC_R1.fastq.gz
 HIC_R2=/home/FCAM/msmith/hiC_data/allhiC_R2.fastq.gz
 PREFIX="intDF011"
+ARIMA_SITES="GATC,GANTC"
+
+##Check quality of HiFi Reads
 
 ##Assemble contigs & convert primary GFA to FASTA
 ./hifiasm.sh "/home/FCAM/msmith/hifiasm_out/hifiasm1_1/${PREFIX}" "${HIC_R1}" "${HIC_R2}" "${HIFI}"
 contig_prim_asm="/home/FCAM/msmith/hifiasm_out/hifiasm1_1/${PREFIX}.asm.hic.p_ctg.fasta"
 ##Align Hi-C reads to contigs and use pairtools to identify contacts, sort and dedup
 ./alnhic_andfilter.sh "${contig_prim_asm}" "${HIC_R1}" "${HIC_R2}" "${PREFIX}"
-chrom_sizes=
-##Scaffold and Quast report
-./pairtools.sh /home/FCAM/msmith/hifiasm_out/hifiasm1_1/intDF011.asm.bp.p_ctg.fasta intDF011
+filtered_bam=/scratch/msmith/
+##Scaffold contig assembly using validated Hi-C contacts
+scaff_outdir=/core/projects/EBP/smith/scaffold
+./scaffold.sh "${contig_prim_asm}" "${filtered_bam}" "${scaff_outdir}/${PREFIX}" "${ARIMA_SITES}" 
