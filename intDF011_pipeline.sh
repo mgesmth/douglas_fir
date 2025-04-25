8616e0041481124fc83740d6b220b44c66bf6c6f
mgesmth <meg8130@student.ubc.ca>
2025-04-25 15:44:45 -0700

    Update intDF011_pipeline.sh

--- intDF011_pipeline.sh
+++ intDF011_pipeline.sh
@@ -14,21 +14,30 @@ HIC_R2=/home/FCAM/msmith/hiC_data/allhiC_R2.fastq.gz
 PREFIX="intDF011"
 ARIMA_SITES="GATC,GANTC"
 
-##Check quality of HiFi Reads
+##Check quality of HiFi Reads and Hi-C Reads
+###Ran with threads=12 and mem=100G
+fastqc_outdir_hifi=/home/FCAM/msmith/fastqc_out_hifi
+fastqc_outdir_hic=/home/FCAM/msmith/fastqc_out_hic
+./fastqc.sh "${fastqc_outdir_hifi}" "${HIFI}"
+./fastqc.sh "${fastqc_outdir_hic}" "${HIC_R1}" "${HIC_R2}"
 
 ##Assemble contigs & convert primary GFA to FASTA
+###Ran with threads=36 and mem=800G
 ./hifiasm.sh "/home/FCAM/msmith/hifiasm_out/hifiasm1_1/${PREFIX}" "${HIC_R1}" "${HIC_R2}" "${HIFI}"
 contig_prim_asm="/home/FCAM/msmith/hifiasm_out/hifiasm1_1/${PREFIX}.asm.hic.p_ctg.fasta"
 
 ##Align Hi-C reads to contigs and use pairtools to identify contacts, sort and dedup
+###Ran with threads=36 and mem=1000G
 ./alnhic_andfilter.sh "${contig_prim_asm}" "${HIC_R1}" "${HIC_R2}" "${PREFIX}"
 filtered_bam=/scratch/msmith/
 
 ##Scaffold contig assembly using validated Hi-C contacts
+###Ran with threads=36 and mem=700G
 scaff_outdir=/core/projects/EBP/smith/scaffold
 ./scaffold.sh "${contig_prim_asm}" "${filtered_bam}" "${scaff_outdir}/${PREFIX}" "${ARIMA_SITES}"
 scaff_prim_asm="${scaff_outdir}/${PREFIX}_scaffolds_final.fa"
 
 ##Run QUAST to get assembly quality statistics
+###Ran with threads=12 and mem=150G
 quast_out=/home/FCAM/msmith/quast_out/1_3
 ./quast.sh "${scaff_prim_asm}" "${quast_out}"
