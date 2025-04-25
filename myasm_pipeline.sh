9fb04ce0eaaa56d19b47105e530c4112bf751e4b
mgesmth <meg8130@student.ubc.ca>
2025-04-25 14:23:36 -0700

    Update myasm_pipeline.sh

--- myasm_pipeline.sh
+++ myasm_pipeline.sh
@@ -5,6 +5,6 @@
 ##Assemble contigs
 ./hifiasm.sh
 ##Align Hi-C reads to contigs
-./aln_hic.sh
+./aln_hic.sh /home/FCAM/msmith/hifiasm_out/hifiasm1_1/intDF011.asm.bp.p_ctg.fasta intDF011
 ##Use pairtools to identify valid ligation junctions, sort, and dedup Hi-C alignments
-./pairtools.sh
+./pairtools.sh /home/FCAM/msmith/hifiasm_out/hifiasm1_1/intDF011.asm.bp.p_ctg.fasta intDF011
