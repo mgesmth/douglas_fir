4d16c2183fd70e392beba25b616f9184ddd9eedb
mgesmth <meg8130@student.ubc.ca>
2025-04-25 14:34:40 -0700

    Update hifiasm.sh

--- hifiasm.sh
+++ hifiasm.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-hifidir=/home/FCAM/msmith/hifi_data
-hiCdir=/home/FCAM/msmith/hiC_data
-outdir=/home/FCAM/msmith/hifiasm_out/hifiasm1_1
+if [[ ( $@ == "--help") ||  $@ == "-h" ]]
+then
+    echo "USAGE: ./hifiasm.sh <GENOMEID> <HIC_R1.fastq.gz> <HIC_R2.fastq.gz> <HIFIREADS.fastq.gz>"
+    echo ""
+    echo "Use hifiasm integrated mode to assemble contigs."
+    echo ""
+    echo "Positional arguments:"
+    echo "<GENOMEID>             A unique string to identify this assembly, prefixed with the path to desired output directory."
+    echo "<HIC_R1.fastq.gz>      The path to Hi-C reads R1."
+    echo "<HIC_R2.fastq.gz>      The path to Hi-C reads R2."
+    echo "<HIFIREADS.fastq.gz>   The path to PacBio HiFi long reads (all files merged into one)."
+    echo ""
+    echo ""
+     exit 0
+fi
 
+#Required Software
 module load Hifiasm/0.20.0
 
-hifiasm -o $outdir/intDF011.asm -t 36 \
---h1 $hiCdir/allhiC_R1.fastq.gz --h2 $hiCdir/allhiC_R2.fastq.gz \
+gid="$1"
+hic1=$2
+hic2=$3
+hifi=$4
+
+
+hifiasm -o ${gid}.asm -t 36 \
+--h1 ${hic1} --h2 ${hic2} \
 -f 39 \
-$hifidir/intDF_allhifi.fastq.gz
+${hifi}
 
 #Transform resulting primary GFA to fasta
 awk '/^S/{print ">"$2"\n"$3}' ${outdir}/intDF011.asm.bp.p_ctg.gfa | fold > ${outdir}/intDF011.asm.bp.p_ctg.fasta
