8dfd96fa1175fdca3811c45384a13057f0a84895
mgesmth <meg8130@student.ubc.ca>
2025-04-28 14:06:45 -0700

    Update quast.sh

--- modules/quast.sh
+++ modules/quast.sh
@@ -3,24 +3,31 @@
 if [[ ( $@ == "--help") ||  $@ == "-h" ]]
 then
     echo ""
-    echo "Usage: ./quast.sh <ASSEMBLY> <OUT_PREFIX>"
+    echo "Usage: ./quast.sh -t <THREADS> -a <ASSEMBLY> -o <OUTDIR>"
     echo ""
     echo "Requirements:"
     echo "	Python > 3"
     echo "	Quast"
     echo ""
-    echo "<ASSEMBLY>        Path to assembly to be assessed."
-    echo "<OUT_PREFIX>      A prefix for output, including path to out directory."
+    echo "-t <THREADS>	    Threads. Default 1."
+    echo "-a <ASSEMBLY>     Path to assembly to be assessed."
+    echo "-o <OUTDIR>       Output directory."
     echo ""
-    echo "I recommend sending everything to scratch as these files can be massive."
     echo ""
 	exit 0
 fi
 
-#Required Software
-module load quast/5.2.0
+OPTSTRING="t:a:o:"
+while getopts ${OPTSTRING} opt
+do
+case ${opt} in
+  t) threads=${OPTARG};;
+  a) asm=${OPTARG};;
+  o) outdir=${OPTARG};;
+  ?)
+    echo "invalid option: ${OPTARG}"
+    exit 1 ;;
+  esac
+done
 
-asm=$1
-out=$2
-
-python3 quast.py -t 12 --split-scaffolds --large -o ${out} ${asm}
+python3 quast.py -t ${threads} --split-scaffolds --large -o ${outdir} ${asm}
