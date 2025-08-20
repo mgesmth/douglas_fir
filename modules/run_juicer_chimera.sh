e8f4fc37e0569dd1b7db169fee88d885f6022185
Meg Smith <meg8130@student.ubc.ca>
2025-08-20 07:24:12 -0700

    Create run_juicer_chimera.sh

---
+++ modules/run_juicer_chimera.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH -J run_juicer_chimeric
+#SBATCH -p general
+#SBATCH -q general
+#SBATCH -c 6
+#SBATCH -n 1
+#SBATCH --mem=20G
+#SBATCH --array=[0-299]
+#SBATCH -o %x.%A.%a.out
+#SBATCH -e %x.%A.%a.err
+
+set -e
+
+echo "[M]: Host Name: `hostname`"
+
+module load samtools/1.19
+module load bwa/0.7.17
+module load java-sdk/1.8.0_92
+
+home=/home/FCAM/msmith
+core=/core/projects/EBP/smith
+scratch=/scratch/msmith
+gid="intdf137"
+site="Arima"
+threads=6
+jd=${scratch}/juicer_formanualcur
+
+#The juicer.sh SLURM version script wouldn't work well for the cluster I worked on,
+#so I modified the CPU juicer.sh script version and did it in several steps
+#In order to accelerate the process.
+#This script (juicer_justchimeric.sh) is literally just the chimeric read handling step.
+
+export SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID
+
+echo "[M]: Beginning juicer run."
+cd ${jd}
+#Okay - now run juicer (CPU version, modified for better handling of large files
+#i.e. storing sam files as bam files and decompressing through a pipe to various scripts
+${jd}/scripts/juicer_justchimeric.sh -f -g "$gid" -d "${jd}/work/intdf137" -s "$site" -S chimeric \
+-p references/intdf137.chrom.sizes -y restriction_sites/intdf137_Arima.txt \
+-z references/interior_primary_final.fa -D "$jd" -t "$threads"
+if [[ $? -e 0 ]] ; then
+  echo "[M]: Juicer complete! Bye."
+  date
+  exit 0
+else
+  echo "[E]: Juicer chimeria handling failed. Exit code $?"
+  date
+  exit 1
+fi
